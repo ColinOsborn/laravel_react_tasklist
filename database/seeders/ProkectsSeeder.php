6be14d00a475897ab62bccf889189c5a0486b233
Colin Osborn <colin.osborn@gmail.com>
2024-06-13 22:42:36 -0600

    completed setup for database seeders

---
+++ database/seeders/ProkectsSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Seeder;
+
+class ProkectsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 1; $i <= 5; $i++) {
+            Project::create([
+                'name' => "Project $i"
+            ]);
+        }
+    }
+}
